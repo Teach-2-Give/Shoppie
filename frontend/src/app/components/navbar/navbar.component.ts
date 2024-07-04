d2978d8a97103d1ba3b270061fd19f09310a12ef
victorpreston <prestonvictor25@gmail.com>
2024-07-04 21:24:54 +0300

    feat(shoppie): frontend complete with a few designs and improvements to follow.

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,30 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHome, faSearch, faHeart, faShoppingCart, faUserCircle, faUserPlus, faUser } from '@fortawesome/free-solid-svg-icons';
+import { RouterLink } from '@angular/router';
+import { SearchService } from '../../services/search.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule, FontAwesomeModule, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  searchTerm:string='';
+  faUserCircle = faUserCircle;
+  faUserPlus = faUserPlus;
+  faUser = faUser;
+  searchTerm: string = '';
 
-  handlesearch(){
-    console.log('searching for:',this.searchTerm)
+
+  constructor(library: FaIconLibrary, private searchService: SearchService) {
+    library.addIcons(faHome, faSearch, faHeart, faShoppingCart);
+  }
+
+  handleSearch() {
+    this.searchService.setSearchTerm(this.searchTerm);
   }
 }
